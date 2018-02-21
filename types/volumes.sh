c16c1491afd8d26633a709d93f9c7e036b50d8c2
Samuel Vandamme <samuel@sava.be>
2018-02-21 23:00:59 +0100

    Added volume backup

--- types/volumes.sh
+++ types/volumes.sh
@@ -1,2 +1,26 @@
 #!/usr/bin/env bash
 
+# Create a list of all containers we need to backup
+# This is done by looking for containers with label
+containers=$(docker ps -q --filter "label=VOLUME_BACKUP")
+
+# Loop list and take backups
+for id in $containers; do
+    # Get container name
+    name=$(docker ps --filter "id=$id" --format "{{.Names}}")
+    echo "Preparing backup for $name"
+    mkdir -p $ROOT/$name/volumes/
+
+    # Get volumes to backup
+    volumes=$(docker inspect --format "{{ index .Config.Labels \"VOLUME_BACKUP\"}}" $name | tr "," "\n")
+
+    # Backup volumes
+    for volume in $volumes; do
+        source=$(docker volume ls -f name=$volume --format "{{.Mountpoint}} {{.Name}}" | grep "$volume$" | awk '{print $1}')
+
+        echo "Backup of $source to $dest"
+        mkdir -p $ROOT/$name/volumes/$volume
+
+        docker run --rm -v $volume:/src:ro -v $PWD/$ROOT/$name/volumes/$volume:/dst busybox cp -av /src /dst
+    done
+done

45364acd2711248a2da56972e204749d9a1edb56
Samuel Vandamme <samuel@sava.be>
2018-05-20 12:08:11 +0200

    Improvement to testing script

--- test/run-dev.sh
+++ test/run-dev.sh
@@ -6,4 +6,4 @@ docker build -t backup-test .
 popd
 
 # Run with bash for testing
-docker run -it -v /var/run/docker.sock:/var/run/docker.sock -e DEBUG=true -v $PWD:/src backup-test bash
+docker run -it -v "$PWD/../types/:/types/" -v /var/run/docker.sock:/var/run/docker.sock -v /:/host/:ro -e DEBUG=true -v $PWD:/src backup-test bash
